a58943a727bd7685edb81dda7a6b14e389ddb23a
evanjd711 <40526586+evanjd711@users.noreply.github.com>
2018-06-24 01:53:00 -0700

    Added some login functionality

---
+++ javascript/login.js
@@ -0,0 +1,8 @@
+function check(form) {
+	if(form.uname,value == "yeet" && form.pwd.value == "123") {
+	window.open('../general/index.html');
+	}
+else {
+	alert("The username and password you entered don't match.")
+	}
+}
